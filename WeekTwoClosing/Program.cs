c489f9c68e10d4ba3d3592bb39d59b3d4db5f74f
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-08 15:00:08 +0300

    ,

--- WeekTwoClosing/Program.cs
+++ WeekTwoClosing/Program.cs
@@ -10,6 +10,29 @@ namespace WeekTwoClosing
     {
         static void Main(string[] args)
         {
+            //1 - Aşağıdaki çıktıyı yazan bir program.
+            //Merhaba
+            //Nasılsın ?
+            //İyiyim
+            //Sen nasılsın ?
+            //2 - Bir adet metinsel , bir adet tam sayı verisi tutmak için 2 adet değişken tanımlayınız.Bunların değerlerini atayıp , ekrana yazdırınız.
+            //3 - Rastgele bir sayı üretip , ekrana yazdırınız.
+            //4 - Rastgele bir sayı üretip , bunun 3'e bölümünden kalanı ekrana yazdırınız.
+            //5 - Kullanıcıya yaşını sorup, 18'den büyükse "+" küçükse "-" yazdıran bir uygulama.
+            //6 - Ekrana 10 defa " Sen Vodafone gibi anı yaşarken , ben Turkcell gibi seni her yerde çekemem." yazan bir uygulama.
+            //7 - Kullanıcıdan 2 adet metinsel değer alıp "Gülse Birsel" , "Demet Evgar" , bunların yerlerini değiştiriniz
+            //8 - Değer döndürmeyen ismi BenDegerDondurmem olan bir metot tanımlayınız.Ekrana "Ben değer döndürmem , benim bir karşılığım yok , beni değişkene atamaya çalışma" yazsın.
+            //9 - İki sayıyı alıp bunların toplam değerini geriye döndüren bir metot yazınız.
+            //10 - Kullanıcıdan true ya da false değeri alıp string bir değer dönen bir metot tanımlayınız.
+            //11 - 3 Kişinin yaşlarını alıp en yaşlı olanının yaş bilgisini dönen bir metot yazınız.
+            //12 - Kullanıcıdan sınırsız sayıda sayı alıp, bunlardan en büyüğünü ekrana yazdıran ve aynı zamanda geriye dönen bir metot yazınız.
+            //13 - Bir metot yardımıyla kullanıcıdan alınan 2 ismin yerlerini değiştiren uygulamayı yazınız.
+            //14 - Kullanıcıdan alınan sayının tek mi yoksa çift mi olduğu bilgisini(true / false) dönen bir metot.
+            //15 - Kullanıcıdan alınan hız ve zaman bilgileriyle , gidilen yolu hesaplayan bir metot yazınız.
+            //16 - Yarıçap bilgisi verilen bir dairenin alanını hesaplayan bir metot yazınız.
+            //17 - "Zaman bir GeRi SayIm" cümlesini alıp, hepsi büyük harf ve hepsi küçük harfle yazdırınız.
+            //18 - "    Selamlar   " metnini bir değişkene atayıp, başındaki ve sonundaki boşlukları siliniz. Kalıcı olarak.
+
             #region 1->
             //Console.WriteLine("Merhaba");
             //Console.WriteLine("Nasılsın ?");
@@ -147,13 +170,13 @@ namespace WeekTwoClosing
         }
         static bool TrueFalse(int sayi)
         {
-            return sayi % 2 ==0;
+            return sayi % 2 == 0;
         }
         static double Calculator(double x, double y)
         {
-            return x * y ;
+            return x * y;
         }
-        static void Converter(ref string id1,ref string id2)
+        static void Converter(ref string id1, ref string id2)
         {
             string temp = id1;
             id1 = id2;
@@ -163,7 +186,7 @@ namespace WeekTwoClosing
         {
             int enBuyuk = 0;
             bool devam = true;
-            while (devam) 
+            while (devam)
             {
                 Console.Write("Lütfen bir sayı giriniz (döngüden çıkmak için 'çıkış' yazın) : ");
                 string sayi = Console.ReadLine();
@@ -183,7 +206,7 @@ namespace WeekTwoClosing
                 {
                     Console.WriteLine("Geçersiz karakter girdiniz.");
                 }
-            
+
 
 
             }
@@ -195,7 +218,7 @@ namespace WeekTwoClosing
             if (y > sonuç)
                 sonuç = y;
             if (z > sonuç)
-                sonuç = z; 
+                sonuç = z;
             return sonuç;
         }
         static string TrueFalseMesaj(bool deger)
@@ -217,5 +240,5 @@ namespace WeekTwoClosing
         {
             Console.WriteLine("\"Ben değer döndürmem , benim bir karşılığım yok , beni değişkene atamaya çalışma\"");
         }
-   }
+    }
 }
